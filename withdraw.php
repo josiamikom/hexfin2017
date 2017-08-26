e42046f2494c010a49544454393db49736d82fc6
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 04:59:52 +0700

    a

--- withdraw.php
+++ withdraw.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'lib/DatabaseHandler.php';
     $db=new DatabaseHandler();
-    $result=$db->OTPList('josiaranda21@gmail.com');
+    $result=$db->OTPLists('josiaranda21@gmail.com');
     var_dump($result);
  ?>
