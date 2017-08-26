dca1c381bbbc7f1270f6929388de50a0da65d95b
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 04:40:50 +0700

    a

--- doWithdraw.php
+++ doWithdraw.php
@@ -2,6 +2,6 @@
 	$data=array('PrimaryID'=>$_POST['from'],'Amount'=>$_POST['Amount']);
 	require_once 'lib/DatabaseHandler.php';
     $db=new DatabaseHandler();
-    $db->getOTP($data);
-    header('location:withdraw.php');
+    var_dump($db->getOTP($data));
+    //header('location:withdraw.php');
  ?>
