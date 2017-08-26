85554a8986c238054bacd0c6232633a66745d089
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 06:29:20 +0700

    a

--- walletHistory.php
+++ walletHistory.php
@@ -2,7 +2,7 @@
 	require_once 'lib/ApiHandler.php';
 	$api=new ApiHandler();
 	$histori=$api->History('2017-08-01','2017-08-27',$_GET['id'],'');
-	if ($histori=='failed') {
+	if ($histori['status']=='failed') {
 		$histori='';
 	}else{
 		$histori=$histori['response'];
