8f18ea0e1cfb5cf33f59dfda6cb4525818ef7b2e
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 06:24:13 +0700

    a

--- walletHistory.php
+++ walletHistory.php
@@ -1,5 +1,5 @@
 <?php 
 	require_once 'lib/ApiHandler.php';
 	$api=new ApiHandler();
-	print_r($api->History('2017-08-26','2017-08-26',$_GET['id'],''));
+	print_r($api->History('2017-08-01','2017-08-27',$_GET['id'],''));
  ?>
