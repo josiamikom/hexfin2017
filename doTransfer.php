8679b8c3d62a6c4d044ee7bd3d55efeadc1a29d1
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 04:04:32 +0700

    a

--- doTransfer.php
+++ doTransfer.php
@@ -1,8 +1,9 @@
 <?php 
+
 print_r($_POST);
-	/*$data=array('PrimaryID'=>'$_POST[from]','Amount'=>'$_POST[Amount]');
+	$data=array('PrimaryID'=>'$_POST[from]','Amount'=>'$_POST[Amount]');
 	$recipient=$_POST['to'];
-	require_once 'lib/DatabaseHandler.php';
+	/*require_once 'lib/DatabaseHandler.php';
     $db=new DatabaseHandler();
     $db->Transfer($data,$recipient);
     header('loaction:wallets.php');*/
