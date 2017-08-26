9e34bbc515679e72be33137fe729d50eaea205f0
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 05:54:46 +0700

    a

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-echo "<pre>";
+/*echo "<pre>";
 require_once 'lib/DatabaseHandler.php';
 	$db=new DatabaseHandler();
 	$data=array('CustomerName'=>'tes1','DateOfBirth'=>'1995-01-21','EmailAddress'=>'josiaranda21@gmail.com','IDNumber'=>'6474','Name'=>'Gas Money','Type'=>'reguler');
-	$topup=array('PrimaryID'=>'10','Amount'=>'900000.00');
+	$topup=array('PrimaryID'=>'10','Amount'=>'900000.00');*/
 	//print_r($db->MakeWallet($data));
-	print_r($db->TopUp($topup));
+	//print_r($db->TopUp($topup));
 //echo date('c');
 	///$otp=array('type'=>'cashout','PrimaryID'=>'9','Amount'=>'50000.00');
 	///print_r($db->getOTP($otp));
@@ -14,5 +14,6 @@ require_once 'lib/DatabaseHandler.php';
 	//print_r($api->History('2017-08-26','2017-08-26','9','500499040'));
 	//$wdraw=array('PrimaryID'=>'9','Amount'=>'10000.00');
 	//print_r($db->Transfer($wdraw,'8'));
-	print_r($db->getWallets('josiaranda21@gmail.com'));
+	//print_r($db->getWallets('josiaranda21@gmail.com'));
+	header('location:dashboard.php');
 ?>
