3029f5c83eb8d485a5b1718f28e25938b759607c
Josi Aranda <josi.a@students.amikom.ac.id>
2017-08-27 07:01:02 +0700

    a

--- walletHistory.php
+++ walletHistory.php
@@ -3,20 +3,21 @@
 	$api=new ApiHandler();
 	$histori=$api->History('2017-08-01','2017-08-27',$_GET['id'],'');
 	if ($histori['status']=='failed') {
-		$histori='';
+		$histori=array('TransactionType'=>'No History','TransactionDate'=>'','Amount'=>'','CurrentBalance'=>'');
 	}else{
 		$histori=$histori['response'];
+		foreach ($histori->getTransactionDetails() as $key => $data) {
+			$history[$key]['TransactionID']=$data->getTransactionID();
+		    $history[$key]['AccountStatementID']=$data->getAccountStatementID();
+		    $history[$key]['TransactionDate']=$data->getTransactionDate();
+		    $history[$key]['TransactionType']=$data->getTransactionType();
+		    $history[$key]['Amount']=$data->getAmount();
+		    $history[$key]['CurrencyCode']=$data->getCurrencyCode();
+		    $history[$key]['Description']=$data->getDescription();
+		    $history[$key]['CurrentBalance']=$data->getCurrentBalance();
+		}
 	}
-	foreach ($histori->getTransactionDetails() as $key => $data) {
-		$history[$key]['TransactionID']=$data->getTransactionID();
-	    $history[$key]['AccountStatementID']=$data->getAccountStatementID();
-	    $history[$key]['TransactionDate']=$data->getTransactionDate();
-	    $history[$key]['TransactionType']=$data->getTransactionType();
-	    $history[$key]['Amount']=$data->getAmount();
-	    $history[$key]['CurrencyCode']=$data->getCurrencyCode();
-	    $history[$key]['Description']=$data->getDescription();
-	    $history[$key]['CurrentBalance']=$data->getCurrentBalance();
-	}
+	
  ?>
 
 <!doctype html>
